0375b58762e23bb0e63b79942bb5f4a9e957d725
Genae <stefan@2stark4you.de>
2018-02-15 17:30:57 +0100

    add parser

---
+++ Parser/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Parser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var reader = new Reader();
+            reader.ReadFile(@"C:\Users\Stefan Stark\Desktop\Wine\stage1.vox");
+            Console.ReadLine();
+        }
+    }
+}
